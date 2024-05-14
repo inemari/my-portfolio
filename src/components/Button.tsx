f81277acba4bcd753a27fd5e994b59ad0412fd0f
Ine Mari Bredesen <inemari.bredesen@no.experis.com>
2024-05-14 09:15:09 +0200

    fixed small byg

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,7 +10,7 @@ type buttonProps = {
 export const Button = (props: buttonProps) => {
     return (
         <a href={props.link} className='w-full group hover:text-white rounded-lg hover:shadow-xs bg-indigo  hover:shadow-gray-400 max-w-[50%] '>
-            <button className="w-full justify-center py-2 px-6 flex flex-row items-center " onClick={onClick}>
+            <button className="w-full justify-center py-2 px-6 flex flex-row items-center " onClick={props.onClick}>
                 <p className='font-bold text-lg'>{props.buttonText}</p>
                 <div className='pl-2 group-hover:hidden'>{props.children}</div>
                 <div className='pl-2 hidden group-hover:flex group-hover:scale-150 ' ><IoArrowForwardOutline /></div>
